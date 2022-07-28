340528329b0edf2514d5365da987af7a19776572
Nazar <Hehera@gmail.com>
2022-07-29 01:27:47 +0300

    communication with the user

--- Script.js
+++ Script.js
@@ -1,26 +1,43 @@
-"use strict";   сучасний режим(строгий)
-let number = 5;
-var name('Ivan');   
-console.log(name);
-const LefrBorderWidth = 1;      
+// "use strict";   сучасний режим(строгий)
+// let number = 5;
+// var name('Ivan');   
+// console.log(name);
+// const LefrBorderWidth = 1;      
 
-const obj = {                        
- a: 50;
-}
-obj.a = 10;
-console.log(obj);
+// const obj = {                        
+//  a: 50;
+// }
+// obj.a = 10;
+// console.log(obj);
   
 
 
 
 
-const obj {             
-    name: "john",
-    age: 25              
-    isMarried: false
-    };
-    console.log(name);
+// const obj {             
+//     name: "john",
+//     age: 25              
+//     isMarried: false
+//     };
+//     console.log(name);
     
     
-    let arr = ['plumb.png','orange.jbg',6,'apple.bmp',{},[]];
-    console.log(arr[3]);
+//     let arr = ['plumb.png','orange.jbg',6,'apple.bmp',{},[]];
+//     console.log(arr[3]);
+
+
+
+
+   
+alert('halo');
+const result =confirm("Here?");
+console.log(result);
+const answer = prompt("Do you want to play?","Yes");
+console.log(answer);
+
+const answers =[];
+answers[0] = prompt('Як ваше імя?','');
+answers[1] = prompt('Яка ваша фамілія','');
+answers[2] = prompt('Яке ваше хоббі','');
+console.log(answers);
+  
