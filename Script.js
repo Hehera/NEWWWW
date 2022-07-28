fd651b9046669558f2bdcdc5c7b6b84a49a804ea
Nazar <Hehera@gmail.com>
2022-07-29 01:18:53 +0300

    Introduction

--- Script.js
+++ Script.js
@@ -1,19 +1,11 @@
+"use strict";   сучасний режим(строгий)
+let number = 5;
+var name('Ivan');   
+console.log(name);
+const LefrBorderWidth = 1;      
 
-const numberOfFilms = +prompt('How much films did you wach?', '');
- 
-personalMovieDB = {
-count: numberOfFilms,
-movies: {},
-actors: {},
-genres: [],
-privat: false
-};
-
-const a = prompt('Один з останніх переглянутих фільмів?', ''),
-      b = prompt('На скільки оціните його?', ''),
-      c = prompt('Один з останніх переглянутих фільмів?', ''),
-      d = prompt('На скільки оціните його', '');
-
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
-      console.log(personalMovieDB);
+const obj = {                        
+ a: 50;
+}
+obj.a = 10;
+console.log(obj);
